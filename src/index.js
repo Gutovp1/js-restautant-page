57a534951793fec3857421bf8798aaba9248baca
Augusto Ventura <paivaav@gmail.com>
2022-07-27 16:37:32 -0300

    Create module rules for css styling and image content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import './style.css';
+import mealImg from './frango-quiabo.jpg';
+
+function includeImage(){
+    const element = document.createElement('div');
+
+    const MyImage = new Image();
+    MyImage.src = mealImg;
+    element.appendChild(MyImage);
+
+    return element;
+}
+
+document.getElementById('content').appendChild(includeImage());
+
 console.log("Simple log from index.js");
