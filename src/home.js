5160f40d37edc3fedc6f85e90eb20b769852587e
Augusto Ventura <paivaav@gmail.com>
2022-07-29 09:52:48 -0300

    Add description home page and bgc img

--- src/home.js
+++ src/home.js
@@ -6,12 +6,16 @@ export default function homeTab(){
         
         const homeTitle = document.createElement('h1');
         homeTitle.classList.add('title');
-        homeTitle.textContent = "Try the mineira food in Shiriu's restaurant.";    
+        homeTitle.textContent = "Shiriu's mineiro restaurant.";    
     
+        const homeCopy = document.createElement('h3');
+        homeCopy.textContent = "Mineira cuisine is well known as the best food in Brazil. Here in Shiriu, you will find the most mineiro typical dishes. Give it a try!";
+
         const homeImage = new Image(400,300);
         homeImage.src = homeImg;
 
         container.appendChild(homeTitle);
+        container.appendChild(homeCopy);
         container.appendChild(homeImage);
         return;
 }
