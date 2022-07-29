9ab253f469aa834e3d04fdfe1139908151830756
Augusto Ventura <paivaav@gmail.com>
2022-07-29 11:09:02 -0300

    Finish menu page and final style of all pages

--- src/home.js
+++ src/home.js
@@ -6,16 +6,20 @@ export default function homeTab(){
         
         const homeTitle = document.createElement('h1');
         homeTitle.classList.add('title');
-        homeTitle.textContent = "Shiriu's mineiro restaurant.";    
-    
+        homeTitle.textContent = "Shiriu's mineiro restaurant";    
+        container.appendChild(homeTitle);
+        
+        
+        const homeContent = document.createElement('div');
         const homeCopy = document.createElement('h3');
         homeCopy.textContent = "Mineira cuisine is well known as the best food in Brazil. Here in Shiriu, you will find the most mineiro typical dishes. Give it a try!";
 
         const homeImage = new Image(400,300);
         homeImage.src = homeImg;
 
-        container.appendChild(homeTitle);
-        container.appendChild(homeCopy);
-        container.appendChild(homeImage);
+        homeContent.appendChild(homeCopy);
+        homeContent.appendChild(homeImage);
+        container.appendChild(homeContent);
+        // homeTab();
         return;
 }
--- src/menu.js
+++ src/menu.js
@@ -8,25 +8,39 @@ export default function menuTab(){
     
     const menuTitle = document.createElement('h1');
     menuTitle.classList.add('title');
-    menuTitle.textContent = 'Regular mineiro Menu';
+    menuTitle.textContent = 'Regular mineiro menu';
     container.appendChild(menuTitle);
+    
 
-    const br = document.createElement('br');
-
+    const dish1 = document.createElement('div');
+    const p1 = document.createElement('h4');
+    p1.textContent = 'Juicy "Frango com Quiabo" (Chicken with Okra)';
+    p1.setAttribute('a','#');
+    dish1.appendChild(p1);
     const mealImage1 = new Image(150,105);
     mealImage1.src = mealImg1;
-    container.appendChild(mealImage1);
-    container.appendChild(br);
+    dish1.appendChild(mealImage1);
 
-
+    const dish2 = document.createElement('div');
+    const p2 = document.createElement('h4');
+    p2.textContent = 'Delicious Feijão Tropeiro (Tropeiro Beans)';
+    dish2.appendChild(p2);
     const mealImage2 = new Image(150,105);
     mealImage2.src = mealImg2;
-    container.appendChild(mealImage2);
-    container.appendChild(br);
+    dish2.appendChild(mealImage2);
     
+    const dish3 = document.createElement('div');
+    const p3 = document.createElement('h4');
+    p3.textContent = 'Warm and soft Pão de Queijo (Cheese Bread)';
+    dish3.appendChild(p3);
     const mealImage3 = new Image(150,105);
     mealImage3.src = mealImg3;
-    container.appendChild(mealImage3);
+    dish3.appendChild(mealImage3);
+
+    
+    container.appendChild(dish1);
+    container.appendChild(dish2);
+    container.appendChild(dish3);
     
     return;
 }
